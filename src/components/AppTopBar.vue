4db1f96489d7ea197be9544e29c032408e988b3c
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2024-06-23 10:47:19 -0300

    Creado componente y añadidos elementos

---
+++ src/components/AppTopBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="topbar">
+        <ul>
+            <li>
+                <a href="#">Logo</a>
+            </li>
+            <li>
+                <a href="#">Titulo</a>
+            </li>
+            <li>
+                <a href="#">redes</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup></script>
