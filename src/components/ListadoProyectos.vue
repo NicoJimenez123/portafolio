1e52b28fb614c379337e737d22fc80c0adf17b0c
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2024-06-28 21:45:47 -0300

    Creado componente para listar los proyectos

---
+++ src/components/ListadoProyectos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="proyecto in proyectos" :key="proyecto.id">
+        {{ proyecto.nombre }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const proyectos = [
+  {id: 1, nombre: 'Proyecto 1'},
+  {id: 2, nombre: 'Proyecto 2'},
+  {id: 3, nombre: 'Proyecto 3'}
+]
+</script>
+
+<style scoped>
+</style>
