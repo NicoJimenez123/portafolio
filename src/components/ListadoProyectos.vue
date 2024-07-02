fd48e395be7c12a4a70155b59f4234fde9b85800
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2024-07-02 20:56:37 -0300

    Modificado listado de proyectos y añadido CSS

--- src/components/ListadoProyectos.vue
+++ src/components/ListadoProyectos.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-    <ul>
+    <ul id="listaProyectos">
       <li v-for="proyecto in proyectos" :key="proyecto.id">
-        {{ proyecto.nombre }}
+        <p class="proyecto proyecto-ambito"> {{ proyecto.ambito }} </p>
+        <p class="proyecto proyecto-nombre"> {{ proyecto.nombre }} </p>
+        <p class="proyecto proyecto-descripcion"> {{ proyecto.descripcion }} </p>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
+const ambitos = {
+  laboral: 'Trabajo',
+  personal: 'Personal',
+}
 const proyectos = [
-  {id: 1, nombre: 'Proyecto 1'},
-  {id: 2, nombre: 'Proyecto 2'},
-  {id: 3, nombre: 'Proyecto 3'}
+  {id: 1, nombre: 'SOCD', descripcion: 'Sistema de Organización y Control de Datos', ambito: ambitos.laboral},
+  {id: 2, nombre: 'Apps de Notas', descripcion: 'App de notas que hice hace 3 años usando ReactJS', ambito: ambitos.personal},
+  {id: 3, nombre: 'Cajero Automático', descripcion: 'App que hice con python, un verano aburrido y solo utilizando mi celular', ambito: ambitos.personal},
 ]
 </script>
 
 <style scoped>
+#listaProyectos {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-gap: 1rem;
+  padding: 1rem;
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+}
 </style>
