5a72e5dcae6ad666b1eac72b9cae3117920c1436
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2024-07-02 23:48:07 -0300

    Modificada forma de obtener los valores del objeto proyecto y añadido CSS para la visibilidad del proyecto

--- src/components/ItemProyecto.vue
+++ src/components/ItemProyecto.vue
@@ -1,11 +1,38 @@
 <template>
-  <div class="item-proyecto">
-    <p class="proyecto proyecto-ambito"> {{ proyecto.ambito }} </p>
-    <p class="proyecto proyecto-nombre"> {{ proyecto.nombre }} </p>
-    <p class="proyecto proyecto-descripcion"> {{ proyecto.descripcion }} </p>
+  <div :class="`item-proyecto ${props.oculto ? 'proyecto-oculto' : 'proyecto-mostrar'}`">
+    <p class="proyecto proyecto-ambito"> {{ props.ambito }} </p>
+    <p class="proyecto proyecto-nombre"> {{ props.nombre }} </p>
+    <p class="proyecto proyecto-descripcion"> {{ props.descripcion }} </p>
+    <p class="proyecto proyecto-tecnologias"> {{ props.tecnologias }} </p>
   </div>
 </template>
 
 <script setup>
-const { proyecto } = defineProps(['proyecto']);
+import { watch } from 'vue';
+
+const props = defineProps({
+  ambito: String,
+  nombre: String,
+  descripcion: String,
+  tecnologias: Array,
+  oculto: Boolean
+})
+
+watch(() => props.oculto, () => {
+  alert(props.oculto)
+})
 </script>
+
+<style>
+.item-proyecto {
+  width: 100%;
+}
+
+.proyecto-mostrar {
+  background-color: red;
+}
+
+.proyecto-oculto {
+  background-color: black;
+}
+</style>
