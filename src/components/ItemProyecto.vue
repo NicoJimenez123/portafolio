e78bf284a58bf84ef23072c9bb12b5183377cbfe
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2024-07-02 22:14:57 -0300

    Creado componente

---
+++ src/components/ItemProyecto.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="item-proyecto">
+    <p class="proyecto proyecto-ambito"> {{ proyecto.ambito }} </p>
+    <p class="proyecto proyecto-nombre"> {{ proyecto.nombre }} </p>
+    <p class="proyecto proyecto-descripcion"> {{ proyecto.descripcion }} </p>
+  </div>
+</template>
+
+<script setup>
+const { proyecto } = defineProps(['proyecto']);
+</script>
